434d586e7306247c3d97f6b90514211e52abbf3f
DevMohdFaiz <shakirfaiz2602@gmail.com>
2026-01-08 19:43:47 +0100

    feat: add configuration for loading environment variables and API key validation

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY not found in environment.")
