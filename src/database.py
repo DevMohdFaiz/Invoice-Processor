b6a13d25737d4bd8d29c57d499fc19b800394245
DevMohdFaiz <shakirfaiz2602@gmail.com>
2026-01-15 18:57:43 +0100

    gave a better name

---
+++ src/database.py
@@ -0,0 +1,233 @@
+import sqlite3
+import pickle
+import os
+import json
+import shutil
+from tqdm import tqdm
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+class ReceiptDB():
+    """Setup the full sql and vector databases for the receipts"""
+
+    def __init__(
+            self,
+            sql_db_path="db/receipts_sql_db",
+            sql_db_name="receipts.db",
+            vector_db_path="db/receipts_vector_db",
+            vector_db_name="receipts",
+            full_receipt_details="assets/full_receipt_details.pkl"
+            ):
+        
+        try:
+            with open(full_receipt_details, "rb") as f:
+                self.full_receipt_details = pickle.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"{full_receipt_details} not found!")
+        self.sql_db_path = sql_db_path
+        self.sql_db = os.path.join(sql_db_path, sql_db_name)
+        self.vector_db_path = vector_db_path
+        self.vector_db_name = vector_db_name
+        self.embeddings = HuggingFaceEmbeddings(
+            model_name= "C:\\Users\HomePC\Desktop\Python\deep_learning_ai\langchain_intro\models\\all-MiniLM-L6-v2",
+            encode_kwargs = {"normalize_embeddings": True}
+        )
+        self.vectorstore= Chroma(
+            embedding_function= self.embeddings,
+            persist_directory= self.vector_db_path,
+            collection_name=self.vector_db_name
+        )
+
+    def create_sql_db(self, db_path=None):
+        """
+        Setup the structure of the sql databse & create necessary tables
+        Args:
+            db_path:str = path of the sql database
+        """
+        if db_path is None:
+            db_path= self.sql_db_path
+
+        if not os.path.exists(db_path): #create the sql db dir if it doesn't exist
+            os.makedirs(db_path)
+
+        with sqlite3.connect(self.sql_db) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS receipts(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    receipt_number TEXT,
+                    date TEXT,
+                    vendor_name TEXT,
+                    tax REAL,
+                    subtotal REAL,
+                    total REAL,
+                    payment_method TEXT,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    additional_information TEXT
+                )
+            """)
+
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS line_items(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                receipt_id INTEGER NOT NULL,
+                description TEXT,
+                quantity INT DEFAULT 1, 
+                unit_price REAL,
+                total_price REAL,
+                FOREIGN KEY (receipt_id) REFERENCES receipts(id)
+            )
+            """)
+
+            cursor.execute("CREATE INDEX IF NOT EXISTS idx_vendor ON receipts(vendor_name)")
+            cursor.execute("CREATE INDEX IF NOT EXISTS idx_total ON receipts(total)")
+            cursor.execute("CREATE INDEX IF NOT EXISTS idx_date ON receipts(date)")
+            cursor.execute("CREATE INDEX IF NOT EXISTS idx_receipt_id ON line_items(receipt_id)")
+
+            conn.commit()
+        print(f"Sql database successfully created at {db_path}")
+
+
+    def store_receipt(self, sql_db=None):
+        """
+        Insert the receipt details to the database
+        """
+        if sql_db is None:
+            sql_db = self.sql_db
+        self.create_sql_db()
+        
+        with sqlite3.connect(self.sql_db) as conn:
+            cursor = conn.cursor()
+            for receipt in tqdm(self.full_receipt_details, desc="Creating receipts db..."):
+                additional_info = receipt.get('additional_information')
+                if isinstance(additional_info, (dict, list)):
+                    additional_info = json.dumps(additional_info)
+                cursor.execute("""
+                        INSERT INTO receipts(
+                        vendor_name, date, total, receipt_number, tax, subtotal, payment_method, additional_information
+                        )
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    """,
+                        (
+                            receipt.get('vendor_name'),
+                            receipt.get('date'),
+                            receipt.get('total', 0),
+                            receipt.get('receipt_number'),
+                            receipt.get('tax'),
+                            receipt.get('subtotal'),
+                            receipt.get('payment_method'),
+                            additional_info,
+                        )
+                    )
+
+                receipt_id = cursor.lastrowid
+
+                items = receipt.get("items", [])
+                for item in items:
+                        cursor.execute("""
+                            INSERT INTO line_items(
+                                receipt_id, description, quantity, unit_price, total_price
+                            )
+                            VALUES (?, ?, ?, ?, ?)
+                        """, 
+                            (
+                                receipt_id,
+                                item.get("description", ""),
+                                item.get("quantity", 1),
+                                item.get("unit_price"),
+                                item.get("total_price", 0)
+                            )          
+                        )
+
+                self.create_vector_db(receipt=receipt, receipt_id=receipt_id)
+
+            conn.commit()
+        print(f"Vector database successfully created at {self.vector_db_path}")
+
+    
+    def create_vector_db(self,  receipt, receipt_id, vector_db_path=None,):
+        """Create embeddings and store to the vector database"""
+        if not vector_db_path:
+            vector_db_path = self.vector_db_path
+
+        if not os.path.exists(vector_db_path): #create the db path if it doesn't exist
+            os.makedirs(vector_db_path)
+
+        detailed_text = f"""
+            Vendor: {receipt.get("vendor_name", "Unknown")}
+            Date: {receipt.get("date")}
+            Items: {[item.get("description") for item in receipt.get("items")]}
+            Total: {receipt.get("total", 1)}
+        """.strip()
+
+        doc = Document(
+            page_content=detailed_text,
+            metadata = {
+                "receipt_id": receipt_id,
+                "date": receipt.get("date"),
+                "vendor": receipt.get("vendor_name", "Unknown"),
+                "total": receipt.get("total", 0)
+            }
+        )
+        self.vectorstore.add_documents([doc])
+        # print(f"{self.vector_db_name} successfully created at {self.vector_db_path}")
+
+    def view_sql_db(self, sql_db=None, receipts_only=True):
+        """View the content of the receipt db"""
+        if sql_db is None:
+            sql_db =self.sql_db
+        with sqlite3.connect(sql_db) as conn:
+            cursor = conn.cursor()
+            if receipts_only:
+                cursor.execute("SELECT * FROM receipts")
+            else:
+                cursor.execute("SELECT * FROM line_items")
+            return cursor.fetchall()
+
+    def delete_sql_db(self, sql_db=None):
+        """Delete the receipts and line_items tables in the sql database"""
+        if sql_db is None:
+            sql_db =self.sql_db
+        with sqlite3.connect(sql_db) as conn:
+            cursor = conn.cursor()
+            cursor.execute("DROP TABLE IF EXISTS receipts")
+            cursor.execute("DROP TABLE IF EXISTS line_items")
+            conn.commit()
+    
+    def delete_vector_db(self, vector_db_path=None):
+        """Delete the vector database"""
+        if vector_db_path is None:
+            vector_db_path= self.vector_db_path
+        
+        if os.path.exists(vector_db_path):
+            shutil.rmtree(vector_db_path)
+            print(f"Vector database at {vector_db_path} successfully deleted")
+        else:
+            print(f"Vector database at {vector_db_path} does not exist")
+
+        
+
+    # def store_reciept_to_vector_db(self):
+        # detailed_text = f"""
+        #     Vendor: {receipt.get("vendor_name", "Unknown")}
+        #     Date: {receipt.get("date")}
+        #     Items: {[item.get("description") for item in receipt.get("items")]}
+        #     Total: {receipt.get("total", 1)}
+        # """.strip()
+        # doc = Document(
+        #     page_content=detailed_text,
+        #     metadata = {
+        #         "receipt_id": receipt_id,
+        #         "date": receipt.get("date"),
+        #         "vendor": receipt.get("vendor_name", "Unknown"),
+        #         "total": receipt.get("total_price", 0)
+        #     }
+        # )
+        # self.vectorstore.add_documents([doc])
+    
+
+class UserDB():
+    """Perform operations on the `users` database"""
+    
